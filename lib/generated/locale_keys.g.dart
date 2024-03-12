4cbdcc13e1fd70f1306d0c89bdabc9fd0ce4a735
ycsong <yo8703@gmail.com>
2024-03-13 07:46:35 +0900

    로컬라이제이션 설정파일 추가

---
+++ lib/generated/locale_keys.g.dart
@@ -0,0 +1,5 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+//flutter pub run easy_localization:generate -S assets/translations -f keys -O lib/generated -o locale_keys.g.dart
+abstract class LocaleKeys {
+  static const login_title = 'login.title';
+}
