4cdd149ef6943ae3d131e2d64acc75bba8806d1a
ycsong <yo8703@gmail.com>
2024-04-10 23:45:40 +0900

    회원가입 완료

--- lib/ui/pages/auth/sign_up/sign_up_complete_page.dart
+++ lib/ui/pages/auth/sign_up/sign_up_complete_page.dart
@@ -1,5 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_my_portfolio/bloc/global/auth/auth_cubit.dart';
 import 'package:flutter_my_portfolio/generated/locale_keys.g.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -23,13 +25,24 @@ class _SignUpCompletePageState extends State<SignUpCompletePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(LocaleKeys.sign_up_id_title.tr()),
+        title: Text(LocaleKeys.sign_up_complete_title.tr()),
       ),
       body: Container(
         width: double.infinity,
         height: double.infinity,
         padding: EdgeInsets.symmetric(horizontal: 30.w),
-        child: const Column(),
+        child: Column(
+          children: [
+            Text(LocaleKeys.sign_up_complete_title.tr()),
+            SizedBox(height: 20.h),
+            Text(LocaleKeys.sign_up_complete_content.tr()),
+            SizedBox(height: 20.h),
+            ElevatedButton(
+              onPressed: () => context.read<AuthCubit>().moveToLoginPage(),
+              child: Text(LocaleKeys.sign_up_content_next.tr()),
+            ),
+          ],
+        ),
       ),
     );
   }
